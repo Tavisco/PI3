0d8a7f9fb0e82000285d8f5b7bf916b82c5b8620
rogerioedeo <rogerioedeo@hotmail.com>
2017-06-07 20:36:07 -0300

    Inclusão de pacote TabelasDTO.

---
+++ src/main/java/br/senac/si/pi3/tabelas/dto/TabelasDTO.java
@@ -0,0 +1,44 @@
+package br.senac.si.pi3.tabelas.dto;
+
+import java.util.Date;
+
+
+public class TabelasDTO {
+    private int id;
+    private String codigoAcao;
+    private String nomeAcao;
+    private Date data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigoAcao() {
+        return codigoAcao;
+    }
+
+    public void setCodigoAcao(String codigoAcao) {
+        this.codigoAcao = codigoAcao;
+    }
+
+    public String getNomeAcao() {
+        return nomeAcao;
+    }
+
+    public void setNomeAcao(String nomeAcao) {
+        this.nomeAcao = nomeAcao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+}
